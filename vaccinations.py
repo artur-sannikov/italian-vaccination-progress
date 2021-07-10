d0b4cb01a461aa53773efbba6a6a2b36c4843b73
artur-sannikov <artur.sannikov96@gmail.com>
2021-07-10 10:15:57 +0200

    major update

--- vaccinations.py
+++ vaccinations.py
@@ -19,18 +19,9 @@ renamed_cols = {
     "fascia_anagrafica": "age_range",
     "sesso_maschile": "males",
     "sesso_femminile": "females",
-    "categoria_operatori_sanitari_sociosanitari": "healthcare_workers",
-    "categoria_personale_non_sanitario": "non_healthcare_workers",
-    "categoria_ospiti_rs+a": "care_home_patients",
-    "categoria_60_69": "category_60_69",
-    "categoria_70_79": "category_70_79",
-    "categoria_soggetti_fragili": "vulnerable_subjects",
-    "categoria_over80": "over_80",
-    "categoria_forze_armate": "armed_forces",
-    "categoria_personale_scolastico": "school_staff",
-    "categoria_altro": "others",
     "prima_dose": "first_dose",
     "seconda_dose": "second_dose",
+    "pregressa_infezione": "previous infection",
     "codice_NUTS1": "NUTS1_code",
     "codice_NUTS2": "NUTS2_code",
     "codice_regione_ISTAT": "ISTAT_regional_code",
@@ -40,9 +31,9 @@ vax_administration = vax_administration.rename(renamed_cols, axis="columns")
 
 vax_administration.to_csv("./data/italian_vaccination.csv", index=False)
 
-# Kaggle authentication
-api = KaggleApi()
-api.authenticate()
+# # Kaggle authentication
+# api = KaggleApi()
+# api.authenticate()
 
-# Create a new version of the data set
-api.dataset_create_version(folder="./data", version_notes="Daily Update")
+# # Create a new version of the data set
+# api.dataset_create_version(folder="./data", version_notes="Daily Update")
